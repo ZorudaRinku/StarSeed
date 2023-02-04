cea048f688fea22635be19f60b79353c01e1fffb
RussAMR <44710713+RussAMR@users.noreply.github.com>
2023-02-03 23:50:15 -0700

    Main menu and Audio Manager Commit

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public Sound[] musicSounds, SfxSounds;
+    public AudioSource musicSource, SfxSource;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic("MainTheme");
+    }
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, x => x.name == name);
+        if (s == null)
+        {
+            Debug.Log("Sound not Found");
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+        
+    }
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(SfxSounds, x => x.name == name);
+        if (s == null)
+        {
+            Debug.Log("Sound not Found");
+        }
+        else
+        {
+            SfxSource.PlayOneShot(s.clip);
+        }
+    }
+}
---
+++ Assets/Scripts/InSceneMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class InSceneMenu
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+        public void RestartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        public void ReturnMenu()
+        {
+            SceneManager.LoadScene(sceneBuildIndex: 0);
+        }
+    }
+}
