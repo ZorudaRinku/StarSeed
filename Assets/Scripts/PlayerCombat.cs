68843da036ea0279c8c201cc9e7512e2182cf50f
RussAMR <44710713+RussAMR@users.noreply.github.com>
2023-02-04 21:20:48 -0700

    Commiting Cooldown Fix

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
     private Animator armsAnimator, bodyAnimator, legsAnimator;
 
-    public int Cooldown = 30;
-    private int CooldownCount = 0;
+    public float Cooldown = 30;
+    private float CooldownCount = 0;
     public GameObject Arrow;
     
 
@@ -40,11 +41,13 @@ public class PlayerCombat : MonoBehaviour
                 bodyAnimator.SetBool("Up", true);
                 if (CooldownCount >= Cooldown)
                 {
+                    AudioManager.instance.PlaySFX("Bow");
                     GameObject arrow = Instantiate(Arrow);
                     arrow.GetComponent<Rigidbody2D>().velocity += Vector2.up * ArrowSpeedMultiplier;
                     arrow.transform.eulerAngles = new Vector3(0, 0, 90);
                     arrow.transform.position = transform.position + new Vector3(0, 1);
                     CooldownCount = 0;
+
                 }
             }
             
@@ -54,6 +57,7 @@ public class PlayerCombat : MonoBehaviour
                 bodyAnimator.SetBool("Left", true);
                 if (CooldownCount >= Cooldown)
                 {
+                    AudioManager.instance.PlaySFX("Bow");
                     GameObject arrow = Instantiate(Arrow);
                     arrow.GetComponent<Rigidbody2D>().velocity += Vector2.left * ArrowSpeedMultiplier;
                     arrow.transform.eulerAngles = new Vector3(0, 0, 180);
@@ -68,6 +72,7 @@ public class PlayerCombat : MonoBehaviour
                 bodyAnimator.SetBool("Down", true);
                 if (CooldownCount >= Cooldown)
                 {
+                    AudioManager.instance.PlaySFX("Bow");
                     GameObject arrow = Instantiate(Arrow);
                     arrow.GetComponent<Rigidbody2D>().velocity += Vector2.down * ArrowSpeedMultiplier;
                     arrow.transform.eulerAngles = new Vector3(0, 0, -90);
@@ -78,102 +83,24 @@ public class PlayerCombat : MonoBehaviour
             
             if (Input.GetKey(KeyCode.RightArrow))
             {
+
                 armsAnimator.SetBool("ShootRight", true);
                 bodyAnimator.SetBool("Right", true);
                 if (CooldownCount >= Cooldown)
                 {
+                    AudioManager.instance.PlaySFX("Bow");
                     GameObject arrow = Instantiate(Arrow);
                     arrow.GetComponent<Rigidbody2D>().velocity += Vector2.right * ArrowSpeedMultiplier;
                     arrow.transform.eulerAngles = new Vector3(0, 0, 0);
                     arrow.transform.position = transform.position + new Vector3(1, 0);
                     CooldownCount = 0;
+
                 }
-            }                
+            }
+            CooldownCount = CooldownCount + (1 * Time.deltaTime);
         }
-        
-        CooldownCount++;
-
-        /*if (CooldownCount >= Cooldown)
-        {
-            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.DownArrow) ||
-                Input.GetKey(KeyCode.RightArrow))
-            {
-                shooting = true;
-            }
-            if (Input.GetKey(KeyCode.UpArrow)) // Shoot Up
-            {
-                shootDirection = "UP";
-                animator.SetBool("ShootUp", true);
-            }
-            else
-            {
-                animator.SetBool("ShootUp", false);
-            }
-
-            if (Input.GetKey(KeyCode.LeftArrow)) // Shoot Left
-            {
-                shootDirection = "LEFT";
-            }
-
-            if (Input.GetKey(KeyCode.DownArrow)) // Shoot Down
-            {
-                shootDirection = "DOWN";
-            }
-
-            if (Input.GetKey(KeyCode.RightArrow)) // Shoot Right
-            {
-                shootDirection = "RIGHT";
-            }
-        }
-        else
-        {
-            shooting = false;
-        }
-
-        CooldownCount++;
-        
-        if (shooting)
-        {
-            if (shootingCount > shootingDelay)
-            {
-                if (shootDirection == "UP")
-                {
-                    GameObject arrow = Instantiate(Arrow);
-                    arrow.GetComponent<Rigidbody2D>().velocity += Vector2.up * ArrowSpeedMultiplier;
-                    arrow.transform.eulerAngles = new Vector3(0, 0, 90);
-                    arrow.transform.position = transform.position + new Vector3(0, 1);
-                    CooldownCount = 0;
-                }
-                else if (shootDirection == "LEFT")
-                {
-                    GameObject arrow = Instantiate(Arrow);
-                    arrow.GetComponent<Rigidbody2D>().velocity += Vector2.left * ArrowSpeedMultiplier;
-                    arrow.transform.eulerAngles = new Vector3(0, 0, 180);
-                    arrow.transform.position = transform.position + new Vector3(-1, 0);
-                    CooldownCount = 0;
-                }
-                else if (shootDirection == "DOWN")
-                {
-                    GameObject arrow = Instantiate(Arrow);
-                    arrow.GetComponent<Rigidbody2D>().velocity += Vector2.down * ArrowSpeedMultiplier;
-                    arrow.transform.eulerAngles = new Vector3(0, 0, -90);
-                    arrow.transform.position = transform.position + new Vector3(0, -1);
-                    CooldownCount = 0;
-                }
-                else if (shootDirection == "RIGHT")
-                {
-                    GameObject arrow = Instantiate(Arrow);
-                    arrow.GetComponent<Rigidbody2D>().velocity += Vector2.right * ArrowSpeedMultiplier;
-                    arrow.transform.eulerAngles = new Vector3(0, 0, 0);
-                    arrow.transform.position = transform.position + new Vector3(1, 0);
-                    CooldownCount = 0;
-                }
-
-                shooting = false;
-                shootingCount = 0;
-            }
-            
-            shootingCount++;
-        }*/
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
     }
 }
