e8ffb1c16c9dc581be2a696a7b8de9d92e724e37
Zoruda <stronger6@gmail.com>
2023-02-05 15:42:39 -0700

    Enemy Spawning

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn
+{
+    public HashSet<Vector2Int> CalculatePositions(HashSet<Vector2Int> floorPositions)
+    {
+        HashSet<Vector2Int> positions = new();
+
+        foreach (var position in floorPositions)
+        {
+            if (!positions.Contains(position))
+            {
+                if (Random.Range(0, 1000) <= 1)
+                {
+                    positions.Add(position);
+                }
+            }
+        }
+
+        return positions;
+    }
+}
